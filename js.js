28ac424fc950f3172a111c4ff951bec9eb66400f
jangidbhanu <36641468+jangidbhanu@users.noreply.github.com>
2018-10-07 00:54:47 +0530

    Code Cleaning

---
+++ js.js
@@ -0,0 +1,8 @@
+<script>
+    document.querySelector('#submit').onclick = function (e) {
+        let form_name = document.querySelector('#name').value;
+        let form_email = document.querySelector('#email').value;
+        window.localStorage.setItem("name", form_name);
+        window.localStorage.setItem("email", form_email);
+    }
+</script>
