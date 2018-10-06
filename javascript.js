d1f52ba2e1d604d8c4ddfa8ad5280df9b7391bd0
urjit shah <urjit64@gmail.com>
2018-10-06 20:56:26 +0530

    Seperated css and js

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.querySelector('#submit').onclick = function (e) {
+        let form_name = document.querySelector('#name').value;
+        let form_email = document.querySelector('#email').value;
+        window.localStorage.setItem("name", form_name);
+        window.localStorage.setItem("email", form_email);
